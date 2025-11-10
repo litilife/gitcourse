29d51acf11a888930e728567adbe2d0dd492e764
litilife <jjimi.mm@gmail.com>
2025-11-10 23:08:44 +0200

    fix typo

--- hello_world.py
+++ hello_world.py
@@ -3,5 +3,5 @@ is this python?
 """
 # just print THE thing
 print("Hello world!")
-print("Hello from feature!")
-print("Hello from release!")
+print("Hello from feature")
+print("Hello from release")
